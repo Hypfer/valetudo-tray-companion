13cfe3047b16f85b4409b4b377f8816eb972c68c
NKnusperer <hendrik@0x00.to>
2022-12-30 11:33:43 +0100

    Log trace messages to console if available

--- valetudo-tray-companion/Program.cs
+++ valetudo-tray-companion/Program.cs
@@ -1,5 +1,5 @@
 ﻿using Avalonia;
-using System;
+using System.Diagnostics;
 using Avalonia.Logging;
 
 namespace valetudo_tray_companion;
@@ -10,12 +10,27 @@ class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        var listener = new ConsoleTraceListener();
+        Trace.Listeners.Add(listener);
+        try
+        {
+            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        }
+        catch (Exception e)
+        {
+            Trace.TraceError("{0:HH:mm:ss.fff} Exception {1}", DateTime.Now, e);
+        }
+        finally
+        {
+            Trace.Flush();
+        }
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
             .UsePlatformDetect()
-            .LogToTrace(LogEventLevel.Debug);
+            .LogToTrace();
 }
